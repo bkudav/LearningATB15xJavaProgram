928a09558f078f8eb390b3d70cf65f194f5cff33
bkudav <kudavbhagyashree@gmail.com>
2025-12-19 18:03:04 +0300

    19-12-2025

---
+++ src/ex_02_Java_Variable_DataTypes/Lab10_Numeric_Data_Types.java
@@ -0,0 +1,24 @@
+package ex_02_Java_Variable_DataTypes;
+
+public class Lab10_Numeric_Data_Types {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 10;
+        int c = 100;
+        long d = 1000;
+        char e = 'C';
+
+        float g = 3.1427272f;
+        double h = 321426237.6778d;
+
+        System.out.println("Display byte value:" + a);
+        System.out.println("Display short value:" + b);
+        System.out.println("Display int value: " + c);
+        System.out.println("Display long value: " + d);
+        System.out.println("Display char value: " + d);
+        System.out.println("Display float value: " + g);
+        System.out.println("Display double value: " + h);
+
+
+    }
+}
