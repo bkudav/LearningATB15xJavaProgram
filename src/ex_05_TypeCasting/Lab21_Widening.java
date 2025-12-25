e4bcae66e9c6f6937deae1e05a0777743bb8195e
bkudav <kudavbhagyashree@gmail.com>
2025-12-25 10:38:58 +0300

    25-12-2025

--- src/ex_05_TypeCasting/Lab21_Widening.java
+++ src/ex_05_TypeCasting/Lab21_Widening.java
@@ -1,4 +1,16 @@
 package ex_05_TypeCasting;
 
 public class Lab21_Widening {
+    public static void main(String[] args) {
+        //TYPE CASTING can be used only with compatible data-types.
+        byte b = 10;
+        int a = b; //Valid Syntax - Implicit - Cast Widening
+        int a1 = (int)b; //Valid Syntax - Explicit - Cast Widening
+        System.out.println(a1);
+
+        byte c = 20;
+        int d = c;
+        int d1 = (int)c;
+        System.out.println(d1);
+    }
 }
--- src/ex_05_TypeCasting/Lab22_Narrowing.java
+++ src/ex_05_TypeCasting/Lab22_Narrowing.java
@@ -1,9 +1,10 @@
 package ex_05_TypeCasting;
 
 public class Lab22_Narrowing {
-    public static void main() {
-
-
-
+    public static void main(String[] args) {
+        /* TYPE CASTING can be used only with compatible data-types. */
+        int val = 300;
+        byte b = (byte)val; //Valid Syntax - Explicit - Cast Narrowing
+        System.out.println(b);
     }
 }
