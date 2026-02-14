5edeff2bd3b9fdb46395a6b833c91b3efea165c8
bkudav <kudavbhagyashree@gmail.com>
2026-02-14 08:21:28 +0300

    10-02-2026

--- src/Test_OOPs_Concepts_10_02/Challenge1.java
+++ src/Test_OOPs_Concepts_10_02/Challenge1.java
@@ -2,15 +2,23 @@ package Test_OOPs_Concepts_10_02;
 
 public class Challenge1 {
     public static void main(String[] args) {
-        Person P = new Person();
+        Person P = new Person("Peter",5);
         System.out.println(P.name);
         System.out.println(P.age);
-
     }
 
     static class Person{
-        String name="Peter";
-        int age=35;
+        String name;
+        int age;
+
+        public Person() {
+            System.out.println("Default Constructor");
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
 
     }
 }
